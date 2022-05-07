63e1cd899c78e36efcbef86d909b0cbf26799b93
Jean Santos <jeaanmichel@protonmail.com>
2022-05-07 14:27:21 -0300

    boilerplate for game screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -2,12 +2,17 @@ import { View, Text, StyleSheet } from "react-native";
 
 const GameScreen = () => {
   return (
-    <View>
+    <View style={styles.screen}>
       <Text>I am a game screen</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
 
 export default GameScreen;
